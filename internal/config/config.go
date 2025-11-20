cf8ec6a0aec5dd2b0eea55fd18eb0adcf5a43a7d
mxmxmx <mv@example.com>
2025-11-21 01:07:38 +0300

    feat/add-env-params

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,25 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 func NewConfig() (*Model, error) {
-	var cfg Model
+	var (
+		cfg Model
+		ok  bool
+	)
 
-	flag.StringVar(&cfg.HTTP.Host, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&cfg.HTTP.ReturningURL, "b", "http://localhost:8080/", "address and port to run server")
+	cfg.HTTP.Host, ok = os.LookupEnv("SERVER_ADDRESS")
+	if !ok {
+		flag.StringVar(&cfg.HTTP.Host, "a", "localhost:8080", "address and port to run server")
+	}
+
+	cfg.HTTP.ReturningURL, ok = os.LookupEnv("BASE_URL")
+	if !ok {
+		flag.StringVar(&cfg.HTTP.ReturningURL, "b", "http://localhost:8080/", "address and port to run server")
+	}
 
 	flag.Parse()
 
