e838a7b4ca6f802c4efea25d65114c96f09cf622
mxmxmx <mv@example.com>
2025-11-25 17:26:11 +0300

    back to iter4 code

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,26 +2,15 @@ package config
 
 import (
 	"flag"
-	"os"
 )
 
 func NewConfig() (*Model, error) {
-	var (
-		cfg Model
-		//ok  bool
-	)
+	var cfg Model
 
-	//godotenv.Load()
+	flag.StringVar(&cfg.HTTP.Host, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&cfg.HTTP.ReturningURL, "b", "http://localhost:8080/", "address and port to run server")
 
-	cfg.HTTP.Host = os.Getenv("SERVER_ADDRESS")
-	if cfg.HTTP.Host == "" {
-		flag.StringVar(&cfg.HTTP.Host, "a", "localhost:8080", "address and port to run server")
-	}
-
-	cfg.HTTP.ReturningURL = os.Getenv("BASE_URL")
-	if cfg.HTTP.ReturningURL == "" {
-		flag.StringVar(&cfg.HTTP.ReturningURL, "b", "http://localhost:8080/", "address and port to run server")
-	}
+	flag.Parse()
 
 	return &cfg, nil
 }
